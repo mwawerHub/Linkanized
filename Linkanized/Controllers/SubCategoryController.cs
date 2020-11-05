8405ee683eca73bee7fcc042314f26c762a4a772
Marcin <marcinwawer4@gmail.com>
2020-11-06 00:33:15 +0100

    Add Subcategory controller and View

---
+++ Linkanized/Controllers/SubCategoryController.cs
@@ -0,0 +1,46 @@
+﻿using Linkanized.Data;
+using Linkanized.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Linkanized.Controllers
+{
+    public class SubCategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public SubCategoryModel Category { get; set; }
+
+        public SubCategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IActionResult> ListPanel(int id)
+        {
+            var categories = await _db.SubCategories.Where(c => c.MainCategoryId == id).ToListAsync();
+            return View(categories);
+        }
+
+        public IActionResult Create()
+        {
+            Category = new SubCategoryModel();
+            return View(Category);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Save()
+        {
+            if (false == ModelState.IsValid)
+                return View(Category);
+
+            _db.SubCategories.Add(Category);
+            _db.SaveChanges();
+
+            return RedirectToAction("ListPanel");
+        }
+    }
+}
