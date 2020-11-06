680994de98cb5bde3b9b031f9e6f2e883c1b7c91
Marcin <marcinwawer4@gmail.com>
2020-11-06 15:41:49 +0100

    Add controler and view for resources

---
+++ Linkanized/Controllers/ResourcesController.cs
@@ -0,0 +1,30 @@
+﻿using Linkanized.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Linkanized.Controllers
+{
+    public class ResourcesController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ResourcesController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IActionResult> All()
+        {
+            var resources = await _db.Resources.ToListAsync();
+            return View("List", resources);
+        }
+
+        public async Task<IActionResult> List(int id)
+        {
+            var resources = await _db.Resources.Where(r => r.SubCategoryId == id).ToListAsync();
+            return View(resources);
+        }
+    }
+}
