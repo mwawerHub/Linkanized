c5a932380016a145edeb4f5d5814c44a29c55b25
Marcin <marcinwawer4@gmail.com>
2020-12-12 17:48:17 +0100

    Created ViewModel fo ResourcesList

---
+++ Linkanized/ViewModels/ResourcesListViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Linkanized.Models;
+
+namespace Linkanized.ViewModels
+{
+    public class ResourcesListViewModel
+    {
+        public int MainCategoryId { get; set; }
+        public MainCategoryModel MainCategory { get; set; }
+        public int SubCategoryId { get; set; }
+        public SubCategoryModel SubCategory { get; set; }
+        public ResourceModel Resource { get; set; }
+    }
+}
