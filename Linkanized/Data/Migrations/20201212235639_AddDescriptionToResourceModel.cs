b149392046f894308691d185a532a8a041dce68b
Marcin <marcinwawer4@gmail.com>
2020-12-13 00:58:08 +0100

    Add migration for Description Property

---
+++ Linkanized/Data/Migrations/20201212235639_AddDescriptionToResourceModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Linkanized.Data.Migrations
+{
+    public partial class AddDescriptionToResourceModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Resources",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Resources");
+        }
+    }
+}
